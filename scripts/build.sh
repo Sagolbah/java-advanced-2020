cbf781941b564c42695b6ec3a8ae2bedf457dc35
Daniil Boger <sagolbah@gmail.com>
2020-06-06 14:06:55 +0300

    specified source encoding

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,4 +5,4 @@ cd "../java-solutions" || exit
 
 fullPath="ru/ifmo/rain/boger/bank"
 
-javac -cp "../../java-advanced-2020/lib/junit-4.11.jar":"../../java-advanced-2020/lib/hamcrest-core-1.3.jar":"." $(find "$fullPath/" -name "*.java")
+javac -cp "../../java-advanced-2020/lib/junit-4.11.jar":"../../java-advanced-2020/lib/hamcrest-core-1.3.jar":"." -encoding utf8 $(find "$fullPath/" -name "*.java")
